271b935e8630a89205985614a10173c3df1db90b
Ander-LIN3S <69191159+Ander-LIN3S@users.noreply.github.com>
2020-09-15 16:53:06 +0200

    Add files via upload

--- R/hello.R
+++ R/hello.R
@@ -1,41 +1,55 @@
-#' Extraer datos de clientes
+#' Extract order data from Prestashop
 #'
-#' Esta función te permite extraer los datos de cualquier endpoint de  prestashop mediante su API.
-#' Para ello, simplemente debes indicar el token, el hostname y los filtros que quieras aplicar.
+#' This function enables you to extract data from the Orders table of Prestashop.
+#' To do so, you just need to indicate the hostname, token and the orders of which you want to extract data from.
 #'
-#' @param token Clave de tu webservice
-#' @param hostname Nombre de tu página web. Por ejemplo, si la URL es https://www.prueba.com/, el hostname es www.prueba.com.
-#' @param id_cliente Vector con el o los ids de clientes a extraer.
-#' @param tam_grupo El número de clientes de los que se pedirá información con cada petición para evitar saturar el servidor.
-#' @param verbose Indica si la función debería indicar cada paso de la extracción de datos.
-#' @return Un dataframe con todos los datos extraidos.
+#' @param token Webservice Key.
+#' @param hostname Hotname of your website. For example, si your URL is https://www.test.com/, the hostname is www.prueba.com. On the other hand, if your URL is https://test.com/ your hostname is test.com.
+#' @param endpoint Indicates the table you want to retrieve data from. The webservice should have permissions.
+#' @param ids A vector including the ids that you want to extract. To extract all the data, set parameter to NULL.
+#' @param group_dim The number of rows that will be retrieved in each API call. Set as default if you will retrieve a lot of data.
+#' @param verbose Whether you want to see the process of extraction.
+#' @return A dataframe with all extracted data
 #' @export
 
-datos_pedido = function(token, hostname,id_cliente, tam_grupo = 100, verbose = FALSE){
+presta_data = function(token, hostname, endpoint ="orders", ids=NULL,  group_dim = 100, verbose = FALSE){
 
-  chunks = split(ids_de_cliente, ceiling(seq_along(ids_de_cliente)/tam_grupo))
+  if(is.null(ids)){
+    url =  paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&output_format=JSON")
+    x = httr::GET(url)
+    x = jsonlite::fromJSON(httr::content(x, type = "text", encoding = "UTF-8"))
+    x = x[[1]]
+    ids = x$id
+    if(verbose==T){print("Extracted all order ids.")}
+  }
+
+  chunks = split(ids, ceiling(seq_along(ids)/group_dim))
+
+  pb <- utils::txtProgressBar(min = 0, max = length(chunks), initial = 0, style = 3)
 
   for(i in 1:length(chunks)){
 
     # Hacemos la petición
     pedidos = paste0(chunks[[i]], collapse = "|")
-    url = paste0("https://",hostname,"/api/orders?ws_key=",token,"&filter[id_customer]=[", pedidos,"]&display=full&output_format=JSON")
+    url = paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&filter[id]=[", pedidos,"]&display=full&output_format=JSON")
+
     x = httr::GET(url)
-    x = jsonlite::fromJSON(jsonlite::content(x, type = "text", encoding = "UTF-8"))
+    x = jsonlite::fromJSON(httr::content(x, type = "text", encoding = "UTF-8"))
     x = x[[1]]
-    x = x[,1:(length(x)-1)]
+    x = x[,!(sapply(x, class) %in% c("data.frame", "matrxi","list"))]
 
     if(i == 1){
-      datos_pedido = x
+      data = x
     } else{
-      datos_pedido = rbind(datos_pedido, x)
+      data = rbind(data, x)
     }
     if(verbose == TRUE){
-      print(paste('Ronda', i, 'terminada. Faltan', length(chunks)-i))
+      print(paste('Round', i, 'finished. Rounds missing: ', length(chunks)-i))
     }
+
+    utils::setTxtProgressBar(pb,i)
   }
 
-  return(datos_pedido)
+  return(data)
 
 }
-
