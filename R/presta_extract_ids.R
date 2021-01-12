f7d204e8498cce2181eca5fa825d521650a1b9d8
Ander-LIN3S <69191159+Ander-LIN3S@users.noreply.github.com>
2021-01-12 13:39:31 +0100

    Changes in documentation + bug fix

--- R/presta_extract_ids.R
+++ R/presta_extract_ids.R
@@ -7,8 +7,8 @@
 #' @param hostname Hotname of your website. For example, si your URL is https://www.test.com/, the hostname is www.test.com. On the other hand, if your URL is https://test.com/ your hostname is test.com.
 #' @param endpoint Indicates the table you want to retrieve data from. The webservice should have permissions.
 #' @param verbose Whether you want to see the process of extraction.
-#' @param date_min Minimum date to get data from. Not required. If used, date_max is also required.
-#' @param date_max Maximum date to get data from. Not required. If used, date_min is also required.
+#' @param date_min Minimum date to get data from in YY-MM-DD format.  Not required. If used, date_max is also required.
+#' @param date_max Maximum date to get data from YY-MM-DD format. Not required. If used, date_min is also required.
 #' @param date_add Column to use for filtering data by date. Defaul is date_add.
 #' @return A vector with all the ids
 #' @export
@@ -18,7 +18,7 @@ presta_extract_ids = function(token, hostname, endpoint ="orders", verbose = FAL
 
   dates_compr = sum(c(is.null(date_min),is.null(date_min)))
   if(dates_compr == 1){stop('Please provide both date_min and dat_max')}
-  if(dates_compr==0){
+  if(dates_compr==2){
     url =  paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&output_format=JSON")
   } else{
     url =  paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&filter[",date_column,"]=[",date_min,",",date_max,"]&date=1&output_format=JSON")
