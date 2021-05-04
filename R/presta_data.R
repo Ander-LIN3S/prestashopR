2a80f4fb625f58cddc36dda089f4e74422d43b7c
Ander-LIN3S <69191159+Ander-LIN3S@users.noreply.github.com>
2021-05-04 11:37:48 +0200

    New functionality
    - Enable to download nested dataframes and matrixes. 
    - Date partitioning for handling large data extraction.

--- R/presta_data.R
+++ R/presta_data.R
@@ -8,42 +8,54 @@
 #' @param endpoint Indicates the table you want to retrieve data from. The webservice should have permissions.
 #' @param ids A vector including the ids that you want to extract. To extract all the data, set parameter to NULL.
 #' @param group_dim The number of rows that will be retrieved in each API call. Set as default if you will retrieve a lot of data.
+#' @param include_nested Whether to include related nested data, such as customer groups.
 #' @param verbose Whether you want to see the process of extraction.
 #' @return A dataframe with all extracted data
 #' @export
 
-presta_data = function(token, hostname, endpoint ="orders",id_column = "id" ,ids=NULL,  group_dim = 100, verbose = FALSE){
+presta_data = function(token, hostname, endpoint ="orders",id_column = "id" ,ids=NULL,  group_dim = 100,
+                       include_nested = F, verbose = FALSE){
 
-  if(is.null(ids)){
-    ids = prestashopR::presta_extract_ids(token, hostname, endpoint, verbose)
-  }
-
-  chunks = split(ids, ceiling(seq_along(ids)/group_dim))
-  pb <- utils::txtProgressBar(min = 0, max = length(chunks), initial = 0, style = 3)
-
-  data = list()
-  for(i in 1:length(chunks)){
-
-    # Hacemos la petición
-    pedidos = paste0(chunks[[i]], collapse = "|")
-    url = paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&filter[",id_column,"]=[", pedidos,"]&display=full&output_format=JSON")
-
-    x = httr::GET(url)
-    x = jsonlite::fromJSON(httr::content(x, type = "text", encoding = "UTF-8"))
-    x = x[[1]]
-    x = x[,!(sapply(x, class) %in% c("data.frame", "matrix","list"))]
-
-    data[[i]] = x
-
-    if(verbose == TRUE){
-      print(paste('Round', i, 'finished. Rounds missing: ', length(chunks)-i))
+    if(is.null(ids)){
+      ids = prestashopR::presta_extract_ids(token, hostname, endpoint, verbose)
     }
 
-    utils::setTxtProgressBar(pb,i)
+    chunks = split(ids, ceiling(seq_along(ids)/group_dim))
+    pb <- utils::txtProgressBar(min = 0, max = length(chunks), initial = 0, style = 3)
+
+    data = list()
+    if(include_nested){related = list()}
+
+    for(i in 1:length(chunks)){
+
+      # Hacemos la petición
+      pedidos = paste0(chunks[[i]], collapse = "|")
+      url = paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&filter[",id_column,"]=[", pedidos,"]&display=full&output_format=JSON")
+
+      x = httr::GET(url)
+      x = jsonlite::fromJSON(httr::content(x, type = "text", encoding = "UTF-8"))
+      x = x[[1]]
+
+      if(include_nested){
+        related[[i]] = x[,sapply(x, class) %in% c("data.frame", "matrix","list")]
+      }
+
+      x = x[,!(sapply(x, class) %in% c("data.frame", "matrix","list"))]
+
+      data[[i]] = x
+
+      if(verbose == TRUE){
+        print(paste('Round', i, 'finished. Rounds missing: ', length(chunks)-i))
+      }
+
+      utils::setTxtProgressBar(pb,i)
+    }
+
+    data = dplyr::bind_rows(data)
+
+    if(include_nested){
+      return(list(data, related))
+    }else{
+      return(data)
+    }
   }
-
-  data = dplyr::bind_rows(data)
-
-  return(data)
-
-}
--- R/presta_extract_ids.R
+++ R/presta_extract_ids.R
@@ -10,23 +10,53 @@
 #' @param date_min Minimum date to get data from in YY-MM-DD format.  Not required. If used, date_max is also required.
 #' @param date_max Maximum date to get data from YY-MM-DD format. Not required. If used, date_min is also required.
 #' @param date_add Column to use for filtering data by date. Defaul is date_add.
+#' @param default_chunks In case too much info is asked, the number of date partitions that will be created for smoother extractions.
 #' @return A vector with all the ids
 #' @export
 #'
 
-presta_extract_ids = function(token, hostname, endpoint ="orders", verbose = FALSE, date_min =NULL, date_max=NULL, date_column = "date_add"){
+presta_extract_ids = function(token, hostname, endpoint ="orders", date_min =NULL, date_max=NULL, date_column = "date_add", verbose = FALSE, default_chunks = 2){
 
-  dates_compr = sum(c(is.null(date_min),is.null(date_min)))
-  if(dates_compr == 1){stop('Please provide both date_min and dat_max')}
-  if(dates_compr==2){
-    url =  paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&output_format=JSON")
-  } else{
-    url =  paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&filter[",date_column,"]=[",date_min,",",date_max,"]&date=1&output_format=JSON")
+    dates_compr = sum(c(is.null(date_min),is.null(date_max)))
+    if(dates_compr == 1){stop('Please provide both date_min and dat_max')}
+    if(dates_compr == 2){
+
+      # Comprobamos si se puede hacer la petición de una sola vez
+      url =  paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&output_format=JSON")
+
+    } else{
+      url =  paste0("https://",hostname,"/api/",endpoint,"?ws_key=",token,"&filter[",date_column,"]=[",date_min,",",date_max,"]&date=1&output_format=JSON")
+    }
+
+    x = httr::GET(url)
+
+    # Si la petición da error, particionamos en dos y volvemos a hacer la peticion
+    if(x$status_code != 200){
+      if(dates_compr ==2){
+
+        # Si no se han pasado fechas, fijamos la fecha actual y el principio de los tiempos
+        start_date = as.Date("2000-01-01")
+        end_date = Sys.Date()
+        fechas = seq(start_date, end_date, by = as.numeric((end_date-start_date)/default_chunks))
+      } else{
+        fechas = seq(date_min, date_max, by = as.numeric((date_max-date_min)/default_chunks))
+      }
+      ids = list()
+      for(i in 1:(length(fechas)-1)){
+
+        if(verbose){message(paste('Too much data. Extracting data from', as.Date(fechas[i]),
+                                  'to',as.Date(fechas[i+1])))}
+
+        ids[[i]] = prueba(token, hostname, endpoint, date_min =fechas[i], date_max=fechas[i+1],
+                          date_column, verbose, default_chunks)
+
+      }
+      x = as.vector(unlist(ids))
+      return(x)
+    }
+    else{
+      x = jsonlite::fromJSON(httr::content(x, type = "text", encoding = "UTF-8"))
+      x = as.vector(unlist(x))
+      return(x)
+    }
   }
-
-  x = httr::GET(url)
-  x = jsonlite::fromJSON(httr::content(x, type = "text", encoding = "UTF-8"))
-  x = as.vector(unlist(x))
-  if(verbose==T){print("Extracted all order ids.")}
-  return(x)
-}
